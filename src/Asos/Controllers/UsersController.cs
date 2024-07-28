40f0c87fba0cdf691c3eb4ad732e91a65423cdc2
Iskandar Kodirov <eiscocyber@gmail.com>
2024-07-28 23:24:37 +0500

    api first version

--- src/Asos/Controllers/UsersController.cs
+++ src/Asos/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
+using Asos.Exceptions;
 using Asos.Interfaces;
 using Asos.Interfaces;
+using Asos.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Asos.Controllers;
@@ -9,7 +11,7 @@ namespace Asos.Controllers;
 public class UsersController(ITokenService tokenService) : BaseController
 {
     [HttpPost("validate")]
-    public async Task<IActionResult> GetToken([FromBody] string token)
+    public async Task<IActionResult> GetToken(string token, ProjectDetails projectDetails)
     {
         var validToken = await tokenService.GetToken(token);
         return Ok(validToken);
