259fcbd995bc8cf2967893ecf05a742964e08e14
Azizbek Abdurahmonov <165587398+abdurahmonov-azizbek@users.noreply.github.com>
2024-07-28 02:12:44 -0700

    models service created

---
+++ src/Asos/Services/Main/Projects/Domain/ModelService.cs
@@ -0,0 +1,9 @@
+﻿namespace Asos.Services.Main.Projects.Domain;
+
+public class ModelService
+{
+    public void SplitModels(IFormFile formFile, string targetModelsPath)
+    {
+        var modelsContent = 
+    }
+}
