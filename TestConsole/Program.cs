681019d2242d483e9d3e34b8c4d3d7f7d2d526cf
Iskandar Kodirov <eiscocyber@gmail.com>
2024-07-28 13:47:07 +0500

    path bugs fixed

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Asos.Services.Main.Projects;
+
 Console.WriteLine("Hello, World!");
+var nigga = new ProjectInitializerService("Nigga");
+nigga.Initialize();
