ced5a492f49473214cda4cc244412fe4c66e07e8
Jurayevkh <jurayevkh@gmail.com>
2024-07-28 14:55:31 +0500

    Model Service done

--- src/Asos/Services/ModelService.cs
+++ src/Asos/Services/ModelService.cs
@@ -1,9 +1,58 @@
-﻿namespace Asos.Services;
+﻿using System.Text.RegularExpressions;
+
+namespace Asos.Services;
 
 public class ModelService
 {
     public void SplitModels(IFormFile formFile, string targetModelsPath)
     {
-        var modelsContent =
+        string inputFilePath = formFile.FileName; 
+        string outputDirectory = targetModelsPath;
+
+        if (!Directory.Exists(outputDirectory))
+        {
+            Directory.CreateDirectory(outputDirectory);
+        }
+
+        string fileContent = File.ReadAllText(inputFilePath);
+        string classPattern = @"(public|private|protected|internal)?\s*class\s+(\w+)\s*{";
+        Regex regex = new Regex(classPattern);
+        MatchCollection matches = regex.Matches(fileContent);
+
+        foreach (Match match in matches)
+        {
+            string className = match.Groups[2].Value;
+            int classStartIndex = match.Index;
+            int classEndIndex = FindClassEndIndex(fileContent, classStartIndex);
+
+            string classContent = fileContent.Substring(classStartIndex, classEndIndex - classStartIndex + 1);
+            string outputFilePath = Path.Combine(outputDirectory, className + ".cs");
+
+            File.WriteAllText(outputFilePath, classContent);
+        }
+
+        //Console.WriteLine("Classlar alohida fayllarga saqlandi.");
+    }
+
+    static int FindClassEndIndex(string content, int startIndex)
+    {
+        int braceCount = 0;
+        for (int i = startIndex; i < content.Length; i++)
+        {
+            if (content[i] == '{')
+            {
+                braceCount++;
+            }
+            else if (content[i] == '}')
+            {
+                braceCount--;
+                if (braceCount == 0)
+                {
+                    return i;
+                }
+            }
+        }
+
+        throw new Exception("Classning tugash nuqtasi topilmadi.");
     }
 }
