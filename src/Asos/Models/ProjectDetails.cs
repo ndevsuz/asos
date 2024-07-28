27d22f488dae9fe8e65da396f4cebc2fc259c65f
Azizbek Abdurahmonov <165587398+abdurahmonov-azizbek@users.noreply.github.com>
2024-07-28 10:38:41 -0700

    project details model added

--- src/Asos/Models/ProjectDetails.cs
+++ src/Asos/Models/ProjectDetails.cs
@@ -10,4 +10,9 @@ public class ProjectDetails
     public Language Language { get; set; }
     public Architecture Architecture { get; set; }
     public IFormFile Models { get; set; } = default!;
+    public string Host { get; set; } = default!;
+    public string Port { get; set; } = default!;
+    public string UserName { get; set; } = default!;
+    public string Password { get; set; } = default!;
+    public string DatabaseName { get; set; } = default!;
 }
